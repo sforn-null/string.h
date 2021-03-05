c5675141f29d510eb192c0ebe6c5f2a3f32f9f25
sforn-null <1641832926@qq.com>
2021-03-05 21:47:39 +0800

    strtok

---
+++ strtok.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	//192.168.100.24 点分十进制
+	//xxxxxxxx@qq.com
+	//strtok
+	//char* strtok(char* str, char* sep)
+	//sep参数是个字符串，定义了用作分隔符的字符集合
+	return 0;
+}
