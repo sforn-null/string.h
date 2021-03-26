1c01a343b1078df785b0887eff1cd50b6af8ceba
sforn-null <1641832926@qq.com>
2021-03-26 23:55:55 +0800

    atoi

---
+++ atoi.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//	const char* p = "12345";
+//	int ret = atoi(p);
+//	//将数字字符串转换成整数，像+和-之类的也是可以的
+//	//遇到非数字字符的时候会停下来
+//	printf("%d\n", ret);
+//	return 0;
+//}
